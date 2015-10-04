95a1efd5ed6f46ff61d1393f6262d49c6934eab3
Stuart B. Wilkins <swilkins@bnl.gov>
2015-10-04 15:31:52 -0400

    First working version for CSX

--- iocBoot/ioctimestamp/st.cmd
+++ iocBoot/ioctimestamp/st.cmd
@@ -10,7 +10,9 @@ dbLoadDatabase("../../dbd/timestamp.dbd",0,0)
 timestamp_registerRecordDeviceDriver(pdbbase) 
 
 ## Load record instances
-dbLoadRecords("../../db/timestamp.db","Sys=XF:23ID1-ES,Dev={FCCD-Timestamp},Inp=XF:23ID1-TS{EVR:1}EvtECnt-I")
+dbLoadRecords("../../db/timestamp.db","Sys=XF:23ID1-ES,Dev={FCCD-TS},Inp=XF:23ID1-TS{EVR:1}EvtCCnt-I,N=100000")
+dbLoadRecords("../../db/timestamp.db","Sys=XF:23ID1-ES,Dev={TO-Inj},Inp=XF:23ID1-TS{EVR:1}EvtACnt-I, N=100000")
+dbLoadRecords("../../db/timestamp.db","Sys=XF:23ID1-ES,Dev={TO-BS},Inp=XF:23ID1-TS{EVR:1}EvtBCnt-I,  N=100000")
 
 iocInit()
 
