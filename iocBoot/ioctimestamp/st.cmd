fce8b653af9cde8b261c831570b2b9e1389bfe1a
K Lauer <klauer@bnl.gov>
2015-10-12 11:17:46 -0400

    Build on RTEMS

--- iocBoot/ioctimestamp/st.cmd
+++ iocBoot/ioctimestamp/st.cmd
@@ -1,8 +1,5 @@
 #!../../bin/linux-x86_64/timestamp
 
-## You may have to change timestamp to something else
-## everywhere it appears in this file
-
 < envPaths
 
 ## Register all support components
@@ -10,9 +7,9 @@ dbLoadDatabase("../../dbd/timestamp.dbd",0,0)
 timestamp_registerRecordDeviceDriver(pdbbase) 
 
 ## Load record instances
-dbLoadRecords("../../db/timestamp.db","Sys=XF:23ID1-ES,Dev={FCCD-TS},Inp=XF:23ID1-TS{EVR:1}EvtCCnt-I,N=100000")
-dbLoadRecords("../../db/timestamp.db","Sys=XF:23ID1-ES,Dev={TO-Inj},Inp=XF:23ID1-TS{EVR:1}EvtACnt-I, N=100000")
-dbLoadRecords("../../db/timestamp.db","Sys=XF:23ID1-ES,Dev={TO-BS},Inp=XF:23ID1-TS{EVR:1}EvtBCnt-I,  N=100000")
+dbLoadRecords("../../db/timestamp.db","Sys=XF:03IDC-ES,Dev={TO-BS},Inp=XF:03IDC-TS{EVR:1}EvtBCnt-I,N=100000")
+
+# dbLoadRecords("../../db/timestamp.db","Sys=XF:23ID1-ES,Dev={TO-Inj},Inp=XF:23ID1-TS{EVR:1}EvtACnt-I, N=100000")
+# dbLoadRecords("../../db/timestamp.db","Sys=XF:23ID1-ES,Dev={FCCD-TS},Inp=XF:23ID1-TS{EVR:1}EvtCCnt-I,N=100000")
 
 iocInit()
-
